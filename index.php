eb8af58f25438de51b54d39887d9081f7ad5a712
evgen <evgeniysmetanyuk@gmail.com>
2022-12-14 17:00:23 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Авторизація та реєстрація</title>
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+
+<form action="" method="">
+    <label for="">Login/Email</label>
+    <input type="text" placeholder="Enter login or email">
+    <br>
+    <label for="">Password</label>
+    <input type="password" placeholder="Enter password">
+    <br>
+    <button>Enter</button>
+    <p>
+        If you don't have an account you can <a href="/register.php">Create one here</a>
+    </p>
+</form>
+
+</body>
+</html>
